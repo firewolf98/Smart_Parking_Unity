54da45629ff05c7725cd53df641f49763f8449bb
Luca Esposito <luca.esposito.98@outlook.it>
2023-11-07 15:07:24 +0100

    Update script movimenti

--- Assets/Scripts/movimenti.cs
+++ Assets/Scripts/movimenti.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class movimenti : MonoBehaviour
 {
-    float moveSpeed=0.5F;
+    float moveSpeed=4.5F;
+    float rotationSpeed = 9.5F;
+
+    Vector3 forward = new Vector3(0, 0, 1);
+    Vector3 backward = new Vector3(0, 0, -1);
     // Start is called before the first frame update
     void Start()
     {
@@ -15,14 +19,14 @@ public class movimenti : MonoBehaviour
     void Update()
     {
         if(Input.GetKey(KeyCode.UpArrow))
-            transform.Translate(Vector3.forward*moveSpeed*Time.deltaTime);
+            transform.Translate(forward*moveSpeed*Time.deltaTime);
         if(Input.GetKey(KeyCode.DownArrow))
-            transform.Translate(-Vector3.forward*moveSpeed*Time.deltaTime);
+            transform.Translate(backward*moveSpeed*Time.deltaTime);
         if(Input.GetKey(KeyCode.LeftArrow))
-            transform.Translate(Vector3.left*moveSpeed*Time.deltaTime);
-        if(Input.GetKey(KeyCode.RightArrow))
-            transform.Translate(-Vector3.left*moveSpeed*Time.deltaTime);
-        if(Input.GetKey(KeyCode.Space))
+            transform.Rotate(Vector3.up, -rotationSpeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.RightArrow))
+            transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.Space))
             transform.Translate(Vector3.up*moveSpeed*Time.deltaTime);
         if(Input.GetKey(KeyCode.LeftShift))
             transform.Translate(-Vector3.up*moveSpeed*Time.deltaTime);
